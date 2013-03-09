ee436b2fe01e8d3141bf3db707e180999612fc9d
Pablo <read.the.disclaimer@gmail.com>
2013-03-09 02:14:11 +0100

    modified arduino code to send WTF? if don't know what u send and android side using a Bluetooth example from android sdk

--- Arduino/arduino_hc_06/arduino_hc_06.ino
+++ Arduino/arduino_hc_06/arduino_hc_06.ino
@@ -1,21 +1,24 @@
-char INBYTE;
-int  LED = 13; // LED on pin 13
+char data;
 
-void setup() {
-  Serial.begin(9600); 
-  pinMode(LED, OUTPUT);
+void setup(){  
+   pinMode(13,OUTPUT);  //stock led on arduino uno board 
+   Serial.begin(9600); //stock baud rate of HC-06 module
+   Serial.flush();
 }
 
-void loop() {
-
-  Serial.println("bla bla bla");
-  while (!Serial.available());   // stay here so long as COM port is empty   
-  
-  INBYTE = Serial.read();        // read next available byte
-
-  if( INBYTE == '0' ) digitalWrite(LED, LOW);  // if it's a 0 (zero) tun LED off
-
-  if( INBYTE == '1' ) digitalWrite(LED, HIGH); // if it's a 1 (one) turn LED on
-
-  delay(50);
+void loop(){
+   
+   while(!Serial.available());
+   data = Serial.read();  
+   if (data == '1'){
+     digitalWrite(13,HIGH);
+     Serial.print("led ON");
+   }
+   else if(data == '0'){       
+     digitalWrite(13,LOW);  
+     Serial.print("led OFF"); 
+   }
+   else{
+     Serial.print("WTF?");    
+   }
 }
